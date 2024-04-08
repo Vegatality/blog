a791bb383e6a7ed2cbb5091fd347ef0a644c2576
Vegatality <vegatality@gmail.com>
2024-04-08 23:01:23 +0900

    feat: icon set

--- blog-config.ts
+++ blog-config.ts
@@ -2,7 +2,7 @@ const config = {
   title: 'Vegatality Blog', // Blog name
   author: 'Vegatality', // Your name
   // eslint-disable-next-line quotes
-  description: "I'm in the process of moving from a Tistory blog...", // Blog description
+  description: "I'm in the process of moving from Tistory blog...", // Blog description
   siteUrl: 'https://vegatality.netlify.app/',
   thumbnail: '/thumbnails/main.png', // Default blog thumbnail. i.e. '/thumbnails/main.png'
   social: {
@@ -16,7 +16,7 @@ const config = {
     utterances: 'https://github.com/Vegatality/blog', // Your GitHub repository
     disqusShortName: '', // Disqus short name
   },
-  icon: 'src/images/gatsby-icon.png', // path of blog icon
+  icon: 'src/images/profile-pic_512x512.png', // path of blog icon
   googleAnalytics: '0', // Google Analytics tracking id
 };
 
